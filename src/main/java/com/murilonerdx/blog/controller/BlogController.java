4d8409830a5d4b774595dae3f6bc562c9c4bd7ae
murilonerdx <mu-silva@outlook.com>
2021-08-06 18:27:16 -0300

    Criando um metodo get para procurar por id

--- src/main/java/com/murilonerdx/blog/controller/BlogController.java
+++ src/main/java/com/murilonerdx/blog/controller/BlogController.java
@@ -4,6 +4,7 @@ import com.murilonerdx.blog.model.Post;
 import com.murilonerdx.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class BlogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = service.findAll();
         mv.addObject("posts",posts);
+        return mv;
+    }
+
+    @RequestMapping(value="/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostsDetails(@PathVariable Long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = service.findById(id);
+        mv.addObject("post",post);
         return mv;
     }
 }
