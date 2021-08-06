71958b3682cfb16d38488054fc055c74790a2ba4
murilonerdx <mu-silva@outlook.com>
2021-08-06 19:00:24 -0300

    Salvando post, criando formulario, e inserindo mensagem de validação

--- src/main/java/com/murilonerdx/blog/model/Post.java
+++ src/main/java/com/murilonerdx/blog/model/Post.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Entity
@@ -19,10 +21,17 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank
     private String titulo;
+
+    @NotBlank
     private String autor;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate data;
+
     @Lob
+    @NotBlank
     private String texto;
 }
